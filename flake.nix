72513ffbfe487cac812fd28236fc15f42f6c5462
monyx <helix.nuked@proton.me>
2026-03-14 19:41:43 +0000

    feat: qol fixes, expand inputs

--- flake.nix
+++ flake.nix
@@ -2,15 +2,46 @@
   description = "Flake for Nix Configuration";
 
   inputs = {
+    ###############
+    # determinate #
+    ###############
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+
     ###########
     # nixpkgs #
     ###########
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # latest unstable version
-    nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*"; # latest stable version
+    # latest unstable version
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    # latest stable version
+    nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+
+    ################
+    # home-manager #
+    ################
+    home-manager = {
+      # latest unstable version
+      url = "https://flakehub.com/f/nix-community/home-manager/0.1";
+      # latest stable version
+      # url = "https://flakehub.com/f/nix-community/home-manager/*";
+
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs dependencies.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ###############
+    # flake-parts #
+    ###############
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
 
     #######
     # nur #
     #######
+    nur = {
+      url = "github:nix-community/nur";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {}: {};
