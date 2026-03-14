c1a6d4da5bed153b21b2526a441f54aa09d6adeb
monyx <helix.nuked@proton.me>
2026-03-14 18:55:14 +0000

    flake: add basic flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake for Nix Configuration";
+
+  inputs = {
+    ###########
+    # nixpkgs #
+    ###########
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # latest unstable version
+    nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*"; # latest stable version
+  };
+
+  outputs = {}: {};
+}
