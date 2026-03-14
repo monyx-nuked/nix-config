049a552fcab124aa3ddee56ef97a36735db94c47
monyx <helix.nuked@proton.me>
2026-03-14 20:24:17 +0000

    flake: abandon flake-parts for some time

--- flake.nix
+++ flake.nix
@@ -49,14 +49,5 @@
     };
   };
 
-  outputs = inputs: 
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
-      systems = [
-        "x86_64-linux"
-      ];
-      imports = [
-        (inputs.import-tree ./parts)
-      ];
-    })
-  ;
+  outputs = {}: {};
 }
