e29a8e22a1b6252f138dac35c03e272d0c662205
monyx <helix.nuked@proton.me>
2026-03-14 19:01:09 +0000

    rename, flake: remove outputs temporarily

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
     ###########
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # latest unstable version
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*"; # latest stable version
+
+    #######
+    # nur #
+    #######
   };
 
-  outputs = {}: {};
 }
