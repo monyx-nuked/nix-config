dfc0602d8f29901c8c55e76976ccf3e7df8c8da4
monyx <helix.nuked@proton.me>
2026-03-15 09:30:48 +0000

    flake: simplify back again

--- flake.nix
+++ flake.nix
@@ -3,31 +3,9 @@
   inputs = {
     # unstable
     nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1";
-    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
   };
-  outputs = inputs@{ flake-parts, ... }:
-  # https://flake.parts/module-arguments.html
-  flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
-    imports = [
-      # Optional: use external flake logic, e.g.
-      # inputs.foo.flakeModules.default
-      ./parts
-    ];
-    flake = {
-      # Put your original flake attributes here.
-    };
-    systems = [
-      # systems for which you want to build the `perSystem` attributes
-      "x86_64-linux"
-      # ...
-    ];
-    perSystem = { config, pkgs, ... }: {
-      # Recommended: move all package definitions here.
-      # e.g. (assuming you have a nixpkgs input)
-      # packages.foo = pkgs.callPackage ./foo/package.nix { };
-      # packages.bar = pkgs.callPackage ./bar/package.nix {
-      #   foo = config.packages.foo;
-      # };
-    };
-  });
+  outputs = inputs@{ nixpkgs, ... }:
+  {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {};
+  };
 }
