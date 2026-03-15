be3d7c1e7ac1fbecc3549fa171e019f862fac160
monyx <helix.nuked@proton.me>
2026-03-15 11:18:58 +0000

    flake: make devshell

--- flake.nix
+++ flake.nix
@@ -1,11 +1,37 @@
 {
-  description = "Flake for NixOS configurations";
+  description = "Flake for My Nix Configurations";
   inputs = {
-    # unstable
     nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1";
+    # nix-index-database = {
+    #   url = "github:nix-community/nix-index-database";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
-  outputs = inputs@{ nixpkgs, ... }:
-  {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {};
+  outputs = {nixpkgs, ...} @ inputs:
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShellNoCC {
+      packages = with pkgs; [
+        # Tools for Nix and NixOS systems
+        alejandra
+        nix-index
+        nix-melt
+        nix-tree
+        nixos-cli
+        nh
+        deadnix
+        # Options seachers
+        manix
+        optnix
+        devenv
+      ];
+      shellHook = ''
+        cat ./res/devshell.txt
+      '';
+    };
   };
 }
