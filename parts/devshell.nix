e323d37d881ad8124612db455bb880be92b5b410
monyx <helix.nuked@proton.me>
2026-03-16 11:13:50 +0000

    devshellL try alias reuse

--- parts/devshell.nix
+++ parts/devshell.nix
@@ -9,9 +9,9 @@
 
       shellHook = ''
         cat ./res/devshell.txt
-        alias devenv='devenv shell --tui "nu --no-std-lib --no-config-file"'
-        alias devenvv='devenv shell --tui'
-        devenv shell --tui "nu --no-std-lib --no-config-file"
+        alias devenvs='devenv shell --tui "nu --no-std-lib --no-config-file"'
+        alias devenv='devenv shell --tui'
+        devenvs
       '';
     };
   };
