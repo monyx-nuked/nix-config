1dbf76866ba5c585a06746ed0c44856fb775bc50
monyx <helix.nuked@proton.me>
2026-03-18 07:23:48 +0000

    flake.lock: delete, fh: fix pkgs

--- src/features/dev/nix/fh.nix
+++ src/features/dev/nix/fh.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  flake.modules.nixos.dev-nix = {...}: {
+  flake.modules.nixos.dev-nix = {pkgs, ...}: {
     environment.systemPackages = [
       inputs.fh.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
