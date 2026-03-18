917c3c88dc31689f8f223a332f2d458002ff1a68
monyx <helix.nuked@proton.me>
2026-03-18 02:06:33 +0000

    Configures Necrozma as WSL host

--- src/_hosts/necrozma/default.nix
+++ src/_hosts/necrozma/default.nix
@@ -1,11 +1,31 @@
 {lib, ...}: {
+  boot = {
+    loader.systemd-boot = {
+      enable = false;
+      memtest86.enable = false;
+    };
+    loader.efi.canTouchEfiVariables = false;
+  };
+
   wsl = {
     enable = true;
     defaultUser = "monyx";
     startMenuLaunchers = true;
     useWindowsDriver = true;
+    wslConf = {
+      boot.systemd = true;
+      interop = {
+        enabled = true;
+        appendWindowsPath = true;
+      };
+      network = {
+        generateHosts = true;
+        generateResolvConf = true;
+      };
+    };
   };
 
+  config.networking.hostName = "necrozma";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   system.stateVersion = "25.11";
 }
