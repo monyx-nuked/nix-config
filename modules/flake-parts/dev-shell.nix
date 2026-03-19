da34ddd6b5a96097691cdeadfd09735b8d6d0f1e
monyx <helix.nuked@proton.me>
2026-03-19 18:31:15 +0000

    dev: expand devshell

--- modules/flake-parts/dev-shell.nix
+++ modules/flake-parts/dev-shell.nix
@@ -1,14 +1,21 @@
 _:
 {
+  # persystem: generate outputs per-system
   perSystem =
     { pkgs, ... }:
     {
+      # devshell: basic devshell with programs
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          just
-          statix
+          actionlint
           deadnix
+          gum
+          just
           nixpkgs-fmt
+          shellcheck
+          shfmt
+          statix
+          treefmt
         ];
       };
     };
