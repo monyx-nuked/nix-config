d4c3bfc01cd202e82aae709878a7b1256e4165b8
monyx <helix.nuked@proton.me>
2026-03-19 17:50:51 +0000

    branch: clean up for restart

--- flake.nix
+++ flake.nix
@@ -1,65 +1,13 @@
 {
-  description = "Flake for My Nix Configurations";
-  nixConfig = {
-    cores = 0;
-    eval-cores = 0; # Determinate Package Manager special feature
-    auto-optimise-store = true;
-    extra-trusted-users = [
-      "@sudo"
-      "@wheel"
-      "monyx" # ADD TRUSTED USERS IN YOUR OWN CONFIG
-    ];
-    extra-experimental-features = [
-      "nix-command"
-      "flakes"
-      "pipe-operators"
-    ];
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://install.determinate.systems"
-      "https://monyx-nix-config.cachix.org"
-      "https://cachix.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      "monyx-nix-config.cachix.org-1:Z8BLQ0CFcXVxq67jpyfgIkHy4/HCghQp4/vsDdNUG9w="
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-    ];
-  };
+  description = "❄ My Nix config for my machines (Primarily NixOS) ";
   inputs = {
     # structure: flake-parts
     flake-parts = {
       url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    # nix's better altarnative
-    determinate.url = "https://flakehub.com/f/determinatesystems/determinate/*";
-    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*";
-    # nixpkgs or repos
+    # nixpkgs: unstable
     nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1";
-    nixpkgs-stable.url = "https://flakehub.com/f/nixos/nixpkgs/*";
-    nur = {
-      url = "github:nix-community/nur";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-    };
-    # modules importer: import-tree
-    import-tree.url = "github:vic/import-tree";
-    # zen-browser
-    zen-browser = {
-      url = "https://flakehub.com/f/youwen5/zen-browser/*";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    secrets.url = "github:monyx-nuked/empty-flake";
-    home-manager = {
-      url = "https://flakehub.com/f/nix-community/home-manager/0.1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-wsl = {
-      url = "github:nix-community/nixos-wsl/main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs =
     { flake-parts, ... }@inputs:
